41f746afdedb1b6200fb44466ccd16fee52806a2
prabhavcb15 <prabhavcb15@gmail.com>
2025-07-04 15:08:13 +0000

    woah wider node menu

--- src/components/maps/node-editor.tsx
+++ src/components/maps/node-editor.tsx
@@ -71,7 +71,7 @@ export function NodeEditor({ isOpen, onOpenChange, node, onUpdate, onDelete }: N
 
   return (
     <Sheet open={isOpen} onOpenChange={onOpenChange}>
-      <SheetContent className="sm:max-w-lg w-[90vw] flex flex-col left-1/2 transform -translate-x-1/2 right-auto">
+      <SheetContent className="md:max-w-3xl lg:max-w-4xl w-[90vw] flex flex-col left-1/2 transform -translate-x-1/2 right-auto">
         <SheetHeader className="px-6 pt-6">
           <SheetTitle>Edit Node</SheetTitle>
           <SheetDescription>Update node properties. Click save when you're done.</SheetDescription>
