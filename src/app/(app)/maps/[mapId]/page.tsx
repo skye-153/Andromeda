f5c5f2790b20a6df1f30f4e061a0cdf3c2691ca3
skye-153 <prabhavcb@gmail.com>
2025-07-10 17:32:28 +0530

    params await fixed :D

--- src/app/(app)/maps/[mapId]/page.tsx
+++ src/app/(app)/maps/[mapId]/page.tsx
@@ -3,16 +3,24 @@ import { getMap } from '@/services/map-service';
 import { notFound } from 'next/navigation';
 
 export const dynamic = 'force-dynamic';
-export default async function MapEditorPage({ params }: { params: { mapId: string } }) {
-    const map = await getMap(params.mapId);
+export const dynamicParams = true;
 
-    if (!map) {
-        notFound();
-    }
-    
-    return (
-        <div className="w-full h-[calc(100vh-120px)] flex flex-col">
-           <MapCanvas map={map} />
-        </div>
-    );
+export default async function MapEditorPage({
+  params,
+}: {
+  params: Promise<{ mapId: string }>;
+}) {
+  const resolvedParams = await params;
+
+  const map = await getMap(resolvedParams.mapId);
+
+  if (!map) {
+    notFound();
+  }
+
+  return (
+    <div className="w-full h-[calc(100vh-120px)] flex flex-col">
+      <MapCanvas map={map} />
+    </div>
+  );
 }
